6cb9c3a08ec39e1e80b00f4a39ab23399a6f6a07
Rishi Broto Chakraborty <rishibroto.chakraborty@tectoro.com>
2023-03-15 14:23:41 +0530

    socket server changes

--- api/controller/messageController.ts
+++ api/controller/messageController.ts
@@ -5,6 +5,7 @@ import randomstring from 'randomstring';
 import appConst from '../constants'
 import * as path from "path";
 import { createBucketAndUpload } from "../commonServices/aws";
+import { Socket } from "socket.io";
 
 
 //add data into database
@@ -12,20 +13,28 @@ import { createBucketAndUpload } from "../commonServices/aws";
 export async function add(req: Request, res: Response): Promise<any> {
     try {
         let ref;
-        if (req.body) {
+        if (req.files) {
             const resp = await createBucketAndUpload(process.env.BUCKET_NAME || '', { ref: 'P1', name: 'P1' }, { ref: 'C1', name: 'C1' }, req.files as any[])
             let obj2 = JSON.parse(req.body.message)
             obj2.messages['files'] = resp
             req.body.message = JSON.parse(JSON.stringify(obj2))
             req.body['ref'] = 'M' + randomstring.generate({ length: 5, charset: 'numeric' })
             console.log("body: ", JSON.stringify(req.body))
+            //Added to DB
             let response = await record.insertOne(req.body)
+
+            // Send through sockets.io
+            const io = req.app.get('io')
+            sendMessagesThroughSocket(req.body,io)
+
             res.status(201).json({ status: appConst.STATUS.SUCCESS, response: null, message: appConst.MESSAGES.AWS_UPLOAD_SUCCESS })
         }
         else {
             const resp = await record.insertOne(req.body)
+            // Send through sockets.io
+            const io = req.app.get('io')
+            sendMessagesThroughSocket(req.body,io)
             res.status(200).json({ status: appConst.STATUS.SUCCESS, response: resp, message: null })
-
         }
     } catch (error: any) {
         console.log(error);
@@ -34,3 +43,9 @@ export async function add(req: Request, res: Response): Promise<any> {
     }
 }
 
+async function sendMessagesThroughSocket(message:any, io: Socket) {
+        io.emit(message.to,message,(response:any)=> {
+            console.log('Message sent via socket: ', response)
+        })
+        console.log('Data emitted.')
+}
--- app.ts
+++ app.ts
@@ -1,18 +1,19 @@
-import express,{Request,Response} from 'express';
-
+import express, { Request, Response } from 'express';
+import { Server } from 'socket.io'
 import bodyPaser from 'body-parser';
 import cors from 'cors';
 import router from './api/router/router';
-const app =express();
+import { createServer } from 'http';
+const app = express();
+const server = createServer(app)
+const io = new Server(server)
+app.set('io',io)
 require('dotenv').config()
-app.use(bodyPaser.urlencoded({extended:true}))
+app.use(bodyPaser.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(cors())
-app.use('/',router);
+app.use('/', router);
+server.listen(process.env.port, () => {
+    console.log(`server listening on port ${process.env.port}`)
+})
 
-async function run() {
-    app.listen(process.env.port,()=>{
-        console.log(`server listening on port ${process.env.port}`)
-    })
-}
-run()
